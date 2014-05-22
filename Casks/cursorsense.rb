d81cc4ad648e152ee0a84239dc6a60d46171977c
ysakmrkm <info@ysakmrkm.com>
2014-05-22 20:24:26 +0900

    Add CursorSense

---
+++ Casks/cursorsense.rb
@@ -0,0 +1,7 @@
+class Cursorsense < Cask
+  url 'http://plentycom.jp/ctrl/files_cs/CursorSense1.1.3.dmg'
+  homepage 'http://plentycom.jp/en/cursorsense/'
+  version 'latest'
+  sha256 :no_check
+  prefpane 'CursorSense.prefPane'
+end
