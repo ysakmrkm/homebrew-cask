aebbde17b87563b12fbf7cae0aa44bc71821044a
ysakmrkm <info@ysakmrkm.com>
2014-07-03 10:39:04 +0900

    Add ImageUp.app

---
+++ Casks/imageup.rb
@@ -0,0 +1,7 @@
+class Imageup < Cask
+  url 'http://download.cockscomb.info/software/imageup/ImageUp.zip'
+  homepage 'http://software.cockscomb.info/imageup/'
+  version '0.7.3'
+  sha256 :no_check
+  link 'ImageUp.app'
+end
