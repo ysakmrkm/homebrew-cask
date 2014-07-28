6b7234eb38ca0f871ae07a4e97db039fd392f3e5
ysakmrkm <info@ysakmrkm.com>
2014-07-28 15:50:59 +0900

    Add iTerm2 version 2.0

---
+++ Casks/iterm.rb
@@ -0,0 +1,7 @@
+class Iterm < Cask
+  url 'http://www.iterm2.com/downloads/stable/iTerm2_v2_0.zip'
+  homepage 'http://www.iterm2.com/'
+  version '2.0.0'
+  sha256 '6a59e1b96d9037b6ff1b8aaaf7aed5cff572d74d6e58390db16028433769c058'
+  link 'iTerm.app'
+end
