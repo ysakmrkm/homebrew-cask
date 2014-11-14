4cc0ece0408d2353c144e2aab9d92726ee00e5c5
ysakmrkm <info@ysakmrkm.com>
2014-11-14 12:22:00 +0900

    Update XQuartz

--- Casks/xquartz.rb
+++ Casks/xquartz.rb
@@ -1,8 +1,8 @@
 class Xquartz < Cask
-  url 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.6.dmg'
+  url 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.7.dmg'
   homepage 'http://xquartz.macosforge.org/'
-  version '2.7.6'
-  sha256 '02aa3268af0bd7dcd5dfbc10d673f5c6834bba6371a928d2a3fc44a8039b194e'
+  version '2.7.7'
+  sha256 :no_check
   install 'XQuartz.pkg'
   uninstall :quit => 'org.macosforge.xquartz.X11',
             :pkgutil => 'org.macosforge.xquartz.pkg'
